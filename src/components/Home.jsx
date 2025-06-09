75f004a3cf1e9497473b5cfaf7514d035e851e0c
Chandu Amuluru <chanduamuluru@Chandus-MacBook-Air.local>
2025-06-09 13:00:11 +0530

    hero background color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import Button from "../layouts/Button";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center lg:px-32 px-5 text-black bg-black bg-[url('assets/img/hero.jpg')] bg-no-repeat bg-cover bg-opacity-90" >
+    <div className="min-h-screen flex flex-col justify-center items-center lg:px-32 px-5 text-white bg-black bg-[url('assets/img/hero.jpg')] bg-no-repeat bg-cover bg-opacity-90" >
       <div className="w-full lg:w-4/5 space-y-5 mt-10 text-center lg:text-left">
         <h1 className="text-3xl md:text-5xl font-bold leading-tight">
          SBT Speciality Orthopaedic Hospital
